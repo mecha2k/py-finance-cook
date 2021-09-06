be87972375ca68dcd9247751551996145166744d
mecha2k <mecha2k@naver.com>
2021-09-06 10:58:20 +0900

    ch04 (multi factor)

--- src/ch07.py
+++ src/ch07.py
@@ -316,7 +316,11 @@ if __name__ == "__main__":
     cov_mat = cov_mat.values
     weights = cp.Variable(n_assets)
     gamma = cp.Parameter(nonneg=True)
-    portf_rtn_cvx = avg_returns * weights
+    portf_rtn_cvx = avg_returns @ weights
+    # portf_rtn_cvx = cp.multiply(avg_returns, weights)
+    ic(avg_returns)
+    ic(weights)
+    ic(portf_rtn_cvx)
     portf_vol_cvx = cp.quad_form(weights, cov_mat)
     objective_function = cp.Maximize(portf_rtn_cvx - gamma * portf_vol_cvx)
     problem = cp.Problem(objective_function, [cp.sum(weights) == 1, weights >= 0])
